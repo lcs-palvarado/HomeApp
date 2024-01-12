abbb3888e6468d01db6668a5d5dfc2ef30897d76
Pablo Alvarado <palvarado@lcs.on.ca>
2024-01-12 09:40:14 -0500

    Changed colors for second time.

--- HomeApp/ContentView.swift
+++ HomeApp/ContentView.swift
@@ -10,18 +10,21 @@ import SwiftUI
 struct ContentView: View {
     
 // MARK: Stored properties
-    let firstGradientColors = Gradient(colors: [.white, .blue])
-    
+    let firstGradientColors = Gradient(colors: [.white, .lightBlue, .deepPurple, .clear])
+    let secondGradientColors = Gradient(colors:[.yellow, .deepPurple, .clear])
 // MARK: Computed properties
     var body: some View {
         ZStack {
             
             // Background
-            Color.orange
+            Color.black
                 .ignoresSafeArea()
             
             // first gradient
-            RadialGradient(gradient: firstGradientColors, center: .center, startRadius: 100, endRadius: 300)
+            RadialGradient(gradient: firstGradientColors, center: .topLeading, startRadius: 0, endRadius: 700)
+            
+            //Second gradient
+            RadialGradient(gradient: secondGradientColors, center: UnitPoint(x: 1.3, y: 1), startRadius: 0, endRadius: 1250)
             
             // Foreground
             VStack {

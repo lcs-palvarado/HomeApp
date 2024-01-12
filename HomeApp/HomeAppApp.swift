bbd0c31539f8d8a4f4d22d830c03e95a59128ad5
Pablo Alvarado <palvarado@lcs.on.ca>
2024-01-12 08:38:42 -0500

    Initial Commit

---
+++ HomeApp/HomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAppApp.swift
+//  HomeApp
+//
+//  Created by Pablo Alvarado on 12/01/24.
+//
+
+import SwiftUI
+
+@main
+struct HomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
